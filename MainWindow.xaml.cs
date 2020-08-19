97d5ef2149cfca5801871b7fc1f2e589d48d1833
Litrop <litrop@live.cn>
2020-08-19 21:48:20 +0900

    Finish roll.

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,28 +1,96 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using System.ComponentModel;
 using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace wpf
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : Window, INotifyPropertyChanged
     {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private Random random = new Random();
+
+        private Boolean isRolling = false;
+
+        public Boolean IsRolling
+        {
+            get
+            {
+                return isRolling;
+            }
+            set
+            {
+                isRolling = value;
+                PropertyChanged(this, new PropertyChangedEventArgs("RollButtonText"));
+            }
+        }
+
+        public string RollButtonText
+        {
+            get
+            {
+                if (isRolling)
+                {
+                    return "Stop rolling";
+                }
+                else
+                {
+                    return "Start rolling";
+                }
+            }
+        }
+
+        private int endNumber = 99;
+
+        public int EndNumber
+        {
+            get
+            {
+                return endNumber;
+            }
+            set
+            {
+                endNumber = value;
+            }
+        }
+
+        private int randomNumber;
+
+        public int RandomNumber
+        {
+            get
+            {
+                return randomNumber;
+            }
+            set
+            {
+                randomNumber = value;
+                PropertyChanged(this, new PropertyChangedEventArgs("RandomNumber"));
+            }
+        }
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void ToggleRoll(object sender, RoutedEventArgs args)
+        {
+            IsRolling = !IsRolling;
+            Task.Run(Rolling);
+        }
+
+        private async void Rolling()
+        {
+            while (isRolling)
+            {
+                RandomNumber = random.Next(1, endNumber + 1);
+                await Task.Delay(100);
+            }
         }
     }
 }
